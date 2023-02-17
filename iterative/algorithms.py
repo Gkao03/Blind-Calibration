e62c287071f8e3a231805a2abe9acdfc243a4e17
Gkao03 <gorek@andrew.cmu.edu>
2023-02-17 18:45:23 -0500

    make u step class

--- iterative/algorithms.py
+++ iterative/algorithms.py
@@ -2,12 +2,23 @@ import scipy.linalg as linalg
 import numpy as np
 
 
-def u_step(g: np.ndarray, H: np.ndarray, Dx: np.ndarray, Dy: np.ndarray, alpha: float, gamma: float, n: int) -> np.ndarray:
-    vx = np.zeros(n)
-    vy = np.zeros(n)
-    ax = np.zeros(n)
-    ay = np.zeros(n)
-    
-    left = H.T @ H + (alpha / gamma) * (Dx.T @ Dx + Dy.T @ Dy)
-    right = H.T @ g + (alpha / gamma) * (Dx.T @ (vx + ax) + Dy.T @ (vy + ay))
-    u = linalg.inv(left) @ right
+class U_Step:
+    def __init__(self, g: np.ndarray, H: np.ndarray, Dx: np.ndarray, Dy: np.ndarray, alpha: float, gamma: float, n: int):
+        self.vx = np.zeros(n)
+        self.vy = np.zeros(n)
+        self.ax = np.zeros(n)
+        self.ay = np.zeros(n)
+        self.g = g
+        self.H = H
+        self.Dx = Dx
+        self.Dy = Dy
+        self.alpha = alpha
+        self.gamma = gamma
+
+        # calculations
+        self.left = H.T @ H + (alpha / gamma) * (Dx.T @ Dx + Dy.T @ Dy)
+        self.HT_g = H.T @ g
+
+    def step(self) -> np.ndarray:        
+        right = self.HT_g + (self.alpha / self.gamma) * (self.Dx.T @ (self.vx + self.ax) + self.Dy.T @ (self.vy + self.ay))
+        u = linalg.inv(self.left) @ right
