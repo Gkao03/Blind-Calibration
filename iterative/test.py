dd56e9ad7cfb3fb51eb76ced5b0226ca870c8812
Gkao03 <gorek@andrew.cmu.edu>
2023-02-21 14:54:21 -0500

    testing convolution as matrix multiplication

---
+++ iterative/test.py
@@ -0,0 +1,24 @@
+import scipy.linalg as linalg
+import numpy as np
+
+
+def create_toeplitz(matrix, kernel):
+    """Creates toeplitz matrix representing 'valid' convolution of matrix with kernel"""
+    in_height, in_width = matrix.shape
+    kernel_height, kernel_width = kernel.shape
+
+    out_height = in_height - kernel_height + 1
+    out_width = in_width - kernel_width + 1
+
+    print(kernel.shape)
+    print(out_height, out_width)
+
+    pad_kernel = np.pad(kernel, ((max(out_height - kernel_height, 0), 0), (0, max(out_width - kernel_width, 0))), 'constant')
+    return pad_kernel
+
+
+if __name__ == '__main__':
+    I = np.arange(16).reshape((4, 4))
+    F = np.array([[10, 20], [30, 40]])
+
+    print(create_toeplitz(I, F))
