f81aece0ed91fc7f3ccc87ef1b692c2545bc065c
Gkao03 <gorek@andrew.cmu.edu>
2023-03-05 05:20:45 -0500

    init encoder and decoder

---
+++ net/models.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+    def forward(self, x):
+        pass
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+    def forward(self, x):
+        pass
