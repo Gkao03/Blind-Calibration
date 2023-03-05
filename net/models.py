5805ab881d06e47f0168a598be7526f9d66388bb
Gkao03 <gorek@andrew.cmu.edu>
2023-03-05 12:23:39 -0500

    lista

--- net/models.py
+++ net/models.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Encoder(nn.Module):
@@ -16,3 +17,35 @@ class Decoder(nn.Module):
 
     def forward(self, x):
         pass
+
+
+class LISTANet(nn.Module):
+    def __init__(self, input_size, dict_size, num_layers):
+        super(LISTANet, self).__init__()
+        self.input_size = input_size
+        self.dict_size = dict_size
+        self.num_layers = num_layers
+        
+        # Define the weight matrices and bias vectors
+        self.W = nn.Parameter(torch.randn(dict_size, input_size))
+        self.b = nn.Parameter(torch.randn(dict_size, 1))
+        self.Theta = nn.Parameter(torch.randn(num_layers, dict_size, dict_size))
+        self.Psi = nn.Parameter(torch.randn(num_layers, dict_size, dict_size))
+        self.alpha = nn.Parameter(torch.randn(num_layers, dict_size, 1))
+        
+    def forward(self, x):
+        # Flatten the input tensor
+        x = x.view(-1, self.input_size, 1)
+        
+        # Linear transform using the dictionary
+        z = F.linear(x, self.W, self.b)
+        
+        # Iterative calculation of sparse codes
+        for i in range(self.num_layers):
+            z = F.linear(z, self.Theta[i], None) + F.linear(x, self.alpha[i], None)
+            z = F.softshrink(z, lambd=0.1)
+            z = F.linear(z, self.Psi[i], None)
+            z = F.relu(z)
+            
+        # Return the final sparse code
+        return z.view(-1, self.dict_size)
