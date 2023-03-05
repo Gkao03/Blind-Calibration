3d46f8a7d314713881e28d19a943d1aebba69afa
Gkao03 <gorek@andrew.cmu.edu>
2023-03-05 12:27:41 -0500

    analytic lista

--- net/models.py
+++ net/models.py
@@ -93,3 +93,55 @@ class AdaptiveLISTA(nn.Module):
         x_recon = x_recon.view(-1, 1, int(math.sqrt(self.input_size)), int(math.sqrt(self.input_size)))
         
         return x_recon
+
+
+class ALISTANet(nn.Module):
+    def __init__(self, input_size, dict_size, num_layers):
+        super(ALISTANet, self).__init__()
+        
+        self.input_size = input_size
+        self.dict_size = dict_size
+        self.num_layers = num_layers
+        
+        # Initialize the weight matrix W and bias vector b
+        self.W = nn.Parameter(torch.Tensor(dict_size, input_size))
+        self.b = nn.Parameter(torch.Tensor(dict_size, 1))
+        nn.init.kaiming_uniform_(self.W, a=math.sqrt(5))
+        nn.init.zeros_(self.b)
+        
+        # Initialize the thresholding function T
+        self.T = nn.Threshold(0, 0)
+        
+        # Compute the analytical weight matrix A and bias vector c
+        self.A, self.c = self.compute_analytical_weights()
+        
+        # Initialize the ALISTA layers
+        self.layers = nn.ModuleList()
+        for i in range(num_layers):
+            self.layers.append(nn.Linear(dict_size, dict_size))
+        
+    def compute_analytical_weights(self):
+        # Compute the analytical weight matrix A and bias vector c
+        alpha = 1.0 / torch.norm(self.W, p=2)
+        A = alpha * torch.mm(self.W.t(), self.W)
+        c = alpha * torch.mm(self.W.t(), self.b)
+        return A, c
+        
+    def forward(self, x):
+        # Flatten the input tensor
+        x = x.view(-1, self.input_size)
+        
+        # Compute the sparse code using the ALISTA algorithm with analytical weights
+        z = torch.zeros(x.shape[0], self.dict_size).to(x.device)
+        z_prev = z
+        for i in range(self.num_layers):
+            z = self.layers[i](z_prev)
+            z = z + torch.mm(x - torch.mm(z, self.W.t()) - torch.mm(z_prev - z, self.A), self.W) + torch.mm(z_prev - z, self.c)
+            z = self.T(z)
+            z_prev = z
+        
+        # Reconstruct the input from the sparse code and dictionary
+        x_recon = torch.mm(z, self.W) + self.b.t()
+        x_recon = x_recon.view(-1, 1, int(math.sqrt(self.input_size)), int(math.sqrt(self.input_size)))
+        
+        return x_recon
