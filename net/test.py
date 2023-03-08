513dc18dab28b75a99a1a9af0b65441682815b09
Gkao03 <gorek@andrew.cmu.edu>
2023-03-08 18:59:18 -0500

    phase shift translation

--- net/test.py
+++ net/test.py
@@ -6,9 +6,12 @@ from utils import *
 
 if __name__ == '__main__':
     im_arr = read_image("../orig_images/lenna.png", flag=cv2.IMREAD_GRAYSCALE)
-    # np_to_image_save(im_arr, "test.png")
+    # np_to_image_save(im_arr, "test.png")  # original
 
-    im_fft = np.fft.fft2(im_arr)
-    im_fft_shift = np.fft.fftshift(im_fft)
+    # im_fft = np.fft.fft2(im_arr)
+    # im_fft_shift = np.fft.fftshift(im_fft)
     # np_to_image_save(np.log(np.abs(im_fft_shift)), "test_fft.png")
     # np_to_image_save(np.log(np.abs(im_fft)), "test_fft2.png")
+
+    im_phase_shift = translate_by_phase_shift(im_arr, 100*np.pi, 100*np.pi)
+    # np_to_image_save(np.log(np.abs(im_phase_shift)), "test_fft.png")
--- net/utils.py
+++ net/utils.py
@@ -13,3 +13,27 @@ def np_to_image_save(np_array, path):
 def read_image(path, flag):
     im = cv2.imread(path, flag)
     return im
+
+
+def phase_shift(fimage, dx, dy):
+    # Shift the phase of the fourier transform of an image
+    dims = fimage.shape
+    x, y = np.meshgrid(np.arange(-dims[1] / 2, dims[1] / 2), np.arange(-dims[0] / 2, dims[0] / 2))
+
+    kx = -1j * 2 * np.pi * x / dims[1]
+    ky = -1j * 2 * np.pi * y / dims[0]
+
+    shifted_fimage = fimage * np.exp(-(kx * dx + ky * dy))
+
+    return shifted_fimage
+
+
+def translate_by_phase_shift(image, dx, dy):
+    # Get the fourier transform
+    fimage = np.fft.fftshift(np.fft.fftn(image))
+    # Phase shift
+    shifted_fimage = phase_shift(fimage, dx, dy)
+    # Inverse transform -> translated image
+    shifted_image = np.real(np.fft.ifftn(np.fft.ifftshift(shifted_fimage)))
+
+    return shifted_image
