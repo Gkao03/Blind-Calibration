80c45fb54391178c8941aa69e1c933fba0d9e171
Gkao03 <gorek@andrew.cmu.edu>
2023-03-08 16:20:31 -0500

    imageio save instead of PIL (and some testing)

--- net/test.py
+++ net/test.py
@@ -6,4 +6,9 @@ from utils import *
 
 if __name__ == '__main__':
     im_arr = read_image("../orig_images/lenna.png", flag=cv2.IMREAD_GRAYSCALE)
-    np_to_image_save(im_arr, "test.png")
+    # np_to_image_save(im_arr, "test.png")
+
+    im_fft = np.fft.fft2(im_arr)
+    im_fft_shift = np.fft.fftshift(im_fft)
+    # np_to_image_save(np.log(np.abs(im_fft_shift)), "test_fft.png")
+    # np_to_image_save(np.log(np.abs(im_fft)), "test_fft2.png")
--- net/utils.py
+++ net/utils.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 import torch
 import torchvision.transforms as transforms
+import imageio
 from PIL import Image
 
 
 def np_to_image_save(np_array, path):
-    im = Image.fromarray(np_array)
-    im.save(path)
+    imageio.imwrite(path, np_array)
 
 
 def read_image(path, flag):
