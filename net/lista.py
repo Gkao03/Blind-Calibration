f396ba33412aa9c9e60a22acec11f491175d6912
Gkao03 <gorek@andrew.cmu.edu>
2023-03-12 04:01:00 -0400

    lista init

---
+++ net/lista.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+
+class LISTA_Layer(nn.Module):
+    def __init__(self) -> None:
+        super(LISTA_Layer, self).__init__()
+
+    def forward(self, x):
+        pass
