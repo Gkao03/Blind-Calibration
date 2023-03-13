d1d25dfa892e848ecea0eb10b6f53066b63cc4ba
Gkao03 <gorek@andrew.cmu.edu>
2023-03-13 05:29:21 -0400

    refactor again

--- net/lista.py
+++ net/lista.py
@@ -3,13 +3,11 @@ import torch.nn as nn
 
 
 class LISTA_Layer(nn.Module):
-    def __init__(self, Y, X, We, S, theta) -> None:
+    def __init__(self, B, S, shrink) -> None:
         super(LISTA_Layer, self).__init__()
-        self.Y = Y
-        self.X = X
-        self.We = We
+        self.B = B
         self.S = S
-        self.theta = theta
+        self.shrink = shrink
 
-    def forward(self, x):
+    def forward(self, X):
         pass
