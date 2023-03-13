a6c2031b788294a32ee5ec187dae633533aa56ef
Gkao03 <gorek@andrew.cmu.edu>
2023-03-13 03:14:25 -0400

    init some variables

--- net/lista.py
+++ net/lista.py
@@ -3,8 +3,12 @@ import torch.nn as nn
 
 
 class LISTA_Layer(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, A, B, theta, shrink) -> None:
         super(LISTA_Layer, self).__init__()
+        self.A = A
+        self.B = B
+        self.theta = theta
+        self.shrink = shrink
 
     def forward(self, x):
         pass
