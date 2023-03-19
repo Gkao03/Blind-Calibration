ce6fd1ccb746e67b0cc80d31765894b764a59eb7
Gkao03 <gorek@andrew.cmu.edu>
2023-03-19 04:08:28 -0400

    dataset template

--- net/data.py
+++ net/data.py
@@ -1,5 +1,18 @@
 import torch
+from torch.utils.data import DataLoader, Dataset
 import numpy as np
+
+
+class MyDataset(Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+
+    def __getitem__(self, index):
+        return self.data[index], self.labels[index]
+
+    def __len__(self):
+        return len(self.data)
 
 
 def generate_X(m, p, theta):
