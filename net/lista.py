1d2715f52d4fa01a3b255a1c574c4c50877b9dae
Gkao03 <gorek@andrew.cmu.edu>
2023-03-20 11:21:04 -0400

    parameter for lista layer

--- net/lista.py
+++ net/lista.py
@@ -5,8 +5,8 @@ import torch.nn as nn
 class LISTA_Layer(nn.Module):
     def __init__(self, B, S, shrink) -> None:
         super(LISTA_Layer, self).__init__()
-        self.B = B
-        self.S = S
+        self.B = nn.Parameter(B)
+        self.S = nn.Parameter(S)
         self.shrink = shrink
 
     def forward(self, X):
