c062f1dca5bda1770ad764105df18c11b455145f
Gore Kao <gkao@purdue.edu>
2023-03-21 16:07:40 -0400

    main

---
+++ net/main.py
@@ -0,0 +1,13 @@
+from data import *
+from lista import LISTA
+
+
+if __name__ == "__main__":
+    m, n, p = 64, 64 * 4, 1024
+    diag_g = generate_diag_g(m, 2)
+    A = generate_A(m, n)
+
+    dataloader = get_lista_dataloader(diag_g, A, n, p, 0.3, 4)
+
+    for batch_idx, (Y, X) in enumerate(dataloader):
+        print(f"{batch_idx} / {len(dataloader)}")
