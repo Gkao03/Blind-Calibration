7b33a3221ae1862ec2380e4cb78a362ec1659fd2
Gore Kao <gkao@purdue.edu>
2023-03-21 16:08:06 -0400

    fix data X generation dimension in dataset

--- net/data.py
+++ net/data.py
@@ -5,15 +5,15 @@ import numpy as np
 
 
 class MyDataset(Dataset):
-    def __init__(self, diag_g: np.ndarray, A: np.ndarray, m: np.ndarray, p: np.ndarray, theta: float):
+    def __init__(self, diag_g: np.ndarray, A: np.ndarray, n: np.ndarray, p: np.ndarray, theta: float):
         self.diag_g = diag_g  # calibration
         self.A = A  # measurement matrix
-        self.m = m  # dimension of x_i
+        self.n = n  # dimension of x_i
         self.p = p  # number of measurements
         self.theta = theta  # sparsity level 0 < theta < 1
 
     def __getitem__(self, index):
-        X = generate_X(self.m, self.p, self.theta)
+        X = generate_X(self.n, self.p, self.theta)
         Y = self.diag_g @ self.A @ X
         return torch.Tensor(Y), torch.Tensor(X)
 
