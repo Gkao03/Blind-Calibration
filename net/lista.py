38284bad0fc7c3867fe0cf321f2613e848097911
Gkao03 <gorek@andrew.cmu.edu>
2023-03-27 05:51:15 -0400

    split real and imag and recombine

--- net/lista.py
+++ net/lista.py
@@ -46,7 +46,13 @@ class LISTA_Layer(nn.Module):
         X_in, By = input
 
         C = torch.matmul(self.S, X_in) + By
-        X_hat = self.shrink(C)
+        C_real = C.real
+        C_imag = C.imag
+
+        X_hat_real = self.shrink(C_real)
+        X_hat_imag = self.shrink(C_imag)
+        X_hat = torch.complex(X_hat_real, X_hat_imag)
+        
         return X_hat
 
 
