15b0d5efe2b702f3caf470bd3fdc191afb8c4fcc
Gkao03 <gorek@andrew.cmu.edu>
2023-03-26 23:20:25 -0400

    instantiate model

--- net/main.py
+++ net/main.py
@@ -1,13 +1,16 @@
 from data import *
+from config import Args
 from lista import LISTA
 
 
 if __name__ == "__main__":
-    m, n, p = 64, 64 * 4, 1024
-    diag_g = generate_diag_g(m, 2)
-    A = generate_A(m, n)
+    args = Args()
 
-    dataloader = get_lista_dataloader(diag_g, A, n, p, 0.3, 8, collate_fn=collate_function)
+    diag_g = generate_diag_g(args.m, 2)
+    A = generate_A(args.m, args.n)
+
+    dataloader = get_lista_dataloader(diag_g, A, args.n, args.p, args.theta, args.batch_size, collate_fn=collate_function)
+    model = LISTA(A, diag_g, args.lambd, args.num_layers)
 
     for batch_idx, (Y, X) in enumerate(dataloader):
         print(f"{batch_idx} / {len(dataloader)} Y shape: {Y.shape} X shape: {X.shape}")
