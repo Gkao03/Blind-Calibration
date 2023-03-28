79f576c96f89cbf4e718e2d7046c840793d4e480
Gkao03 <gorek@andrew.cmu.edu>
2023-03-28 01:00:26 -0400

    some training loops

--- net/main.py
+++ net/main.py
@@ -16,23 +16,59 @@ if __name__ == "__main__":
     dataloader = get_lista_dataloader(diag_g, A, args.n, args.p, args.theta, args.batch_size, collate_fn=collate_function)
     model = LISTA(A, diag_g, args.lambd, args.num_layers)
     criterion = nn.L1Loss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
 
-    for epoch in range(args.epochs):
-        for batch_idx, (Y, X) in enumerate(dataloader):
-            optimizer.zero_grad()
+    for layer_num in range(args.num_layers + 1):
 
-            # get model output
-            out, _ = model(Y)
+        for iter in range(args.iters_per_layer):
+            print(f"iteration {iter + 1}/{args.iters_per_layer}")
 
-            # get recons
-            recon_layers = model.get_recons()
-            loss = 0
+            for batch_idx, (Y, X) in enumerate(dataloader):
+                optimizer.zero_grad()
 
-            # calculate loss
-            for recon_layer in recon_layers:
-                loss += criterion(recon_layer.get_recon(), X)
+                # get model output
+                out, _ = model(Y)
 
-            # back prop
-            loss.backward()
-            optimizer.step()
+                # calculate loss
+                loss = criterion(out, X)
+
+                # back prop
+                loss.backward()
+                optimizer.step()
+
+        # freeze layer after training loop
+        print(f"freezing layer {layer_num}")
+        for name, param in model.named_parameters():
+            if param.requires_grad and f'layer{layer_num}' in name:
+                param.requires_grad = False
+
+    # training with intermediate recon layers
+    # for epoch in range(args.epochs):
+    #     for batch_idx, (Y, X) in enumerate(dataloader):
+    #         optimizer.zero_grad()
+
+    #         # get model output
+    #         out, _ = model(Y)
+
+    #         # get recons
+    #         recon_layers = model.get_recons()
+    #         loss = 0
+
+    #         # calculate loss
+    #         for recon_layer in recon_layers:
+    #             loss += criterion(recon_layer.get_recon(), X)
+
+    #         # back prop
+    #         loss.backward()
+    #         optimizer.step()
+
+    # temp = []
+    # for name, param in model.named_parameters():
+    #      if 'S' in name:
+    #         temp.append(param.data)
+
+    # print(len(temp))
+    # print(torch.all(torch.eq(temp[-1], temp[-2])))
+
+    # for name, param in model.named_parameters():
+    #     print(name, param.data)
