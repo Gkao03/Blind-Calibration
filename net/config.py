3e9e5f7bc78f45f20eb7a8d3e8430ef2f59d8a8f
Gkao03 <gorek@andrew.cmu.edu>
2023-03-28 05:35:59 -0400

    larger model and training

--- net/config.py
+++ net/config.py
@@ -2,14 +2,14 @@ class Args:
     def __init__(self):
         self.m = 64
         self.n = 64 * 4
-        self.p = 1024
+        self.p = 2048
         self.theta = 0.3
         self.lambd = 0.1
-        self.num_layers = 4
+        self.num_layers = 8
         self.lr = 0.001
-        self.batch_size = 8
+        self.batch_size = 16
         self.random_seed = 2023
         self.epochs = 10
-        self.iters_per_layer = 5
+        self.iters_per_layer = 100
         self.save_dir = "./out"
         self.exp_num = 1
