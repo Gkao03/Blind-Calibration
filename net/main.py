bcfde7482b7cd4bb5c2deb04fad50e159b9ab6a2
Gkao03 <gorek@andrew.cmu.edu>
2023-03-29 02:01:47 -0400

    use an identity for diag g initialization

--- net/main.py
+++ net/main.py
@@ -18,12 +18,13 @@ if __name__ == "__main__":
         os.makedirs(out_dir)
 
     diag_g = generate_diag_g(args.m, 2)
+    diag_g_init = np.eye(args.m)
     A = generate_A(args.m, args.n)
     train_loader = get_lista_dataloader(diag_g, A, args.n, args.p, args.theta, args.batch_size, collate_fn=collate_function)
     test_loader = get_lista_dataloader(diag_g, A, args.n, 512, args.theta, args.batch_size, collate_fn=collate_function)
     
     device = get_device()
-    model = LISTA(A, diag_g, args.lambd, args.num_layers).to(device)
+    model = LISTA(A, diag_g_init, args.lambd, args.num_layers).to(device)
     criterion = nn.L1Loss()
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     scheduler = optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.5, verbose=True)
