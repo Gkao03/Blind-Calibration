e06de3afbd63f7d23cebc639887b29cc9ea13ec6
Gkao03 <gorek@andrew.cmu.edu>
2023-04-23 07:58:11 -0400

    exp 2

--- net/config.py
+++ net/config.py
@@ -15,5 +15,5 @@ class Args:
         self.epochs = 10
         self.epochs_per_layer = 10
         self.save_dir = "out/"
-        self.exp_num = 1
+        self.exp_num = 2
         self.out_dir = os.path.join(self.save_dir, f"exp{self.exp_num}")
